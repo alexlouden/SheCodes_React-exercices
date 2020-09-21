0b9d5dfd210643bafdb318878da7565b8a8692a7
Alex Louden <alex@louden.com>
2020-09-21 18:22:40 +0800

    Update image path

--- src/components/PlayTool/PlayTool.js
+++ src/components/PlayTool/PlayTool.js
@@ -1,19 +1,11 @@
 import React from "react";
 
-import scissors from "./img/scissors.jpg";
-import paper from "./img/paper.jpg";
-import rock from "./img/rock.jpg";
-
 function PlayTool({ tool, toolPicked, onClick }) {
-  // const image = `components/PlayTool/img/${props.tool}.jpg`;
+  const image = require(`./img/${tool}.jpg`);
   return (
     <div className={`card ${tool === toolPicked ? " active" : ""}`}>
       <h3>{tool}</h3>
-      <img
-        src={tool === "scissors" ? scissors : tool === "paper" ? paper : rock}
-        alt={tool}
-        onClick={onClick}
-      />
+      <img src={image} alt={tool} onClick={onClick} />
     </div>
   );
 }
